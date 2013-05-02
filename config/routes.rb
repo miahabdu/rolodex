c74b4fe78ec1b6ed10638a06187d6a58c235e17b
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2013-05-02 14:39:20 -0400

    master setup devise user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Railbus::Application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
+  root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
