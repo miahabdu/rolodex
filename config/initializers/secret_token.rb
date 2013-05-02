99fb955219bbf4ef0417492ad71de77b669b7097
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2013-05-02 13:50:05 -0400

    get git commit to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbus::Application.config.secret_token = 'bc1006209d7847405828f1b75753fb72d0bab2b20cf3679e21df1cc6eda85bf9944369bde0e6332c21bc33eb367be977a2f57e67d17847f346746bf4bfa2e0d6'
