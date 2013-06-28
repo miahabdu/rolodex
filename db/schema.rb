24bac63b84f2c6014867f24ccc387e2320b1cc1a
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2013-06-28 14:18:26 -0400

    add_username make username login work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613181430) do
+ActiveRecord::Schema.define(:version => 20130628181016) do
 
   create_table "cards", :force => true do |t|
     t.integer  "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130613181430) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
