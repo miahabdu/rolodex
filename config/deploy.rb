7a0daa9b2d38b269fced034373fead197be4df61
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2013-07-02 18:25:40 -0400

    master make use sudo true

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "rolodex"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:miahabdu/#{application}.git"
